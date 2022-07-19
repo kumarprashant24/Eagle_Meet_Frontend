ee21edcec6c035b5b7262f0f2e60d2bab0bb86ab
kumarprashant24 <kumar.prashant@tothenew.com>
2022-07-19 17:22:49 +0530

    added mute mic preview functionality to all users

--- src/Components/Meeting.jsx
+++ src/Components/Meeting.jsx
@@ -32,7 +32,7 @@ export default function Meeting({ user }) {
   const [toggleTheme, setToggleTheme] = useState(false);
   let uniqueStreamId = "";
   const peer = new Peer();
-  const [hideAvtar,setHideAvtar] = useState(-1)
+  const [hideAvtar, setHideAvtar] = useState(-1);
 
   const toggleRefresh = () => setRefresh((p) => !p);
   const loadUser = () => {
@@ -84,39 +84,66 @@ export default function Meeting({ user }) {
     checkpeer[data] = call;
   }
   function addVideoStream(user, stream) {
-    setClients((current) => [...current, {stream:stream,zIndex:"-1",user:user}]);
-    setMeetingList((current) => [...current, {streamId:stream.id,user:user}]);
+    setClients((current) => [
+      ...current,
+      { stream: stream, zIndex: "-1", zIndexMic:"-1" , user: user },
+    ]);
+    setMeetingList((current) => [
+      ...current,
+      { streamId: stream.id, user: user },
+    ]);
   }
 
   socket.on("user-disconnected", (data) => {
     if (checkpeer[data.id]) checkpeer[data.id].close();
-    setMeetingList(current =>
-      current.filter(employee => {
+    setMeetingList((current) =>
+      current.filter((employee) => {
         return employee.streamId !== data.streamId;
-      }),
+      })
     );
     document.getElementById(data.streamId).remove();
-  
   });
 
-  const mute = () => {
-    // if (isMuted === false) {
-    //   console.log(   myStreamVideo.getTracks());
-    //   myStreamVideo.getTracks()[0].enabled = false;
-    //   setIsMuted(true);
-    // } else {
-    //   myStreamVideo.getTracks()[0].enabled = true;
-    //   setIsMuted(false);
-    // }
+  const mute = (streamId, streamVideo) => {
+    if (isMuted === false) {
+      
+      // myStreamVideo.getTracks()[0].enabled = false;
+      socket.emit("off-mic", {
+        room: uid,
+        streamId: streamId,
+        zIndexMic: "1",
+      });
+      setIsMuted(true);
+    } else {
+      // myStreamVideo.getTracks()[0].enabled = true;
+      socket.emit("off-mic", {
+        room: uid,
+        streamId: streamId,
+        zIndexMic: "-1",
+      });
+      setIsMuted(false);
+    }
   };
+  socket.off("set-default-mic").on("set-default-mic", (data) => {
+    console.log(data);
+    setClients((current) =>
+      current.map((obj) => {
+        if (obj.stream.id === data.streamId) {
+          console.log(obj);
+          return { ...obj, zIndexMic: data.zIndexMic };
+        }
+        return obj;
+      })
+    );
+  });
   const stopVideo = (streamId, streamVideo) => {
     if (isDisplay === false) {
       console.log("off");
       socket.emit("off-video", {
         room: uid,
         streamId: streamId,
-      
-        zIndex:"1"
+
+        zIndex: "1",
       });
       myStreamVideo.getTracks()[0].enabled = false;
       setHideAvtar(10);
@@ -127,27 +154,23 @@ export default function Meeting({ user }) {
       socket.emit("off-video", {
         room: uid,
         streamId: streamId,
-        zIndex:"-1"
+        zIndex: "-1",
       });
-  
+
       myStreamVideo.getTracks()[0].enabled = true;
-      
+
       setIsDisplay(false);
     }
-
-  
   };
   socket.off("set-default-video").on("set-default-video", (data) => {
- 
-    setClients(current =>
-        current.map(obj => {
-          if (obj.stream.id === data.streamId) {
-            return {...obj, zIndex:data.zIndex};
-          }
-          return obj;
-        }),
-      );
-
+    setClients((current) =>
+      current.map((obj) => {
+        if (obj.stream.id === data.streamId) {
+          return { ...obj, zIndex: data.zIndex };
+        }
+        return obj;
+      })
+    );
   });
   const closeMeeting = () => {
     socket.emit("call-ended", {
@@ -165,17 +188,15 @@ export default function Meeting({ user }) {
     document.getElementById(data.streamId).remove();
   });
 
-
-
   return (
     <>
-      <div className="bg-dark" style={{height:"100%"}}>
+      <div className="bg-dark" style={{ height: "100%" }}>
         <Modal room={uid} user={user} url={window.location.href}></Modal>
         <MeetingList meetingList={meetingList} />
         <Chat room={uid} user={user}></Chat>
 
         <div
-          style={{ height:"90vh", overflow: "auto" }}
+          style={{ height: "90vh", overflow: "auto" }}
           className="position-relative container p-0 "
         >
           <div className="grid-system mt-2" id="rows">
@@ -195,90 +216,97 @@ export default function Meeting({ user }) {
                       autoPlay
                     ></video>
                     <div className=" position-absolute top-0 w-100 h-100  d-flex justify-content-center align-items-center">
-                      <img src={element.user.picture_url} className="avtar " style={{zIndex:`${element.zIndex}`}}/>
+                      <img
+                        src={element.user.picture_url}
+                        className="avtar "
+                        style={{ zIndex: `${element.zIndex}` }}
+                      />
+                    </div>
+                    <div className="position-absolute top-0 end-0" style={{ zIndex: `${element.zIndexMic}` }}>
+                    <i className="fa-solid fa-microphone-slash text-white me-2 mt-2 corner   round-img bg-secondary"  ></i>
                     </div>
                     <div className="d-flex position-absolute bottom-0 mb-3 ms-2">
-                      <img src={element.user.picture_url} className="tag"/>
-                    <div className="text-white fw-bold d-flex align-items-center ms-2">{element.user.firstname+ " " + element.user.lastname}</div>
+                      <img src={element.user.picture_url} className="tag" />
+                      <div className="text-white fw-bold d-flex align-items-center ms-2">
+                        {element.user.firstname + " " + element.user.lastname}
+                      </div>
                     </div>
                   </div>
                 </>
               );
             })}
           </div>
-
-        
         </div>
         <div className="d-flex justify-content-center mt-2 position-absolute bottom-0 w-100 ">
-            <div
-              className="d-flex row justify-content-between  p-2 container-fluid"
-              style={{ background: "rgba(0, 0, 0, 0.5)" }}
-            >
-              <div className="d-flex align-items col justify-content-center ms-3">
-                <div className="d-flex">
-                  <div data-bs-toggle="modal" data-bs-target="#exampleModal">
-                    <div>
-                      <i className="fa-solid  fa-shapes corner fa-2x  round-img  d-flex justify-content-center align-items-center text-white"></i>
-                    </div>
-                  </div>
+          <div
+            className="d-flex row justify-content-between  p-2 container-fluid"
+            style={{ background: "rgba(0, 0, 0, 0.5)" }}
+          >
+            <div className="d-flex align-items col justify-content-center ms-3">
+              <div className="d-flex">
+                <div data-bs-toggle="modal" data-bs-target="#exampleModal">
                   <div>
-                    <div>
-                      <i className="fa-solid  fa-circle-info corner fa-2x  round-img  d-flex justify-content-center align-items-center text-white"></i>
-                    </div>
+                    <i className="fa-solid  fa-shapes corner fa-2x  round-img  d-flex justify-content-center align-items-center text-white"></i>
                   </div>
                 </div>
-              </div>
-              <div className="d-flex align-items-center justify-content-center col">
-                <div
-                  className="me-2"
-                  onClick={() => {
-                    mute();
-                  }}
-                >
-                  {isMuted ? (
-                    <i className="fa-solid  fa-microphone-slash call-end bg-danger round-img  d-flex justify-content-center align-items-center text-white"></i>
-                  ) : (
-                    <i className="fa-solid  fa-microphone unmute bg-secondary round-img  d-flex justify-content-center align-items-center text-white"></i>
-                  )}
-                </div>
-                <div
-                  className="me-2"
-                  onClick={() => {
-                    stopVideo(myStreamId, myStreamVideo);
-                  }}
-                >
-                  {isDisplay ? (
-                    <i className="fa-solid  fa-video-slash call-end bg-danger round-img  d-flex justify-content-center align-items-center text-white"></i>
-                  ) : (
-                    <i className="fa-solid  fa-video call-end bg-secondary round-img  d-flex justify-content-center align-items-center text-white"></i>
-                  )}
-                </div>
-                <div onClick={closeMeeting}>
-                  <i className="fa-solid  fa-phone-slash call-end bg-danger round-img  d-flex justify-content-center align-items-center text-white"></i>
-                </div>
-              </div>
-              <div className="d-flex col justify-content-center me-3">
-                <div
-                  data-bs-toggle="offcanvas"
-                  data-bs-target="#offcanvasRight"
-                  aria-controls="offcanvasRight"
-                >
+                <div>
                   <div>
-                    <i className="fa-solid  fa-user corner fa-2x  round-img  d-flex justify-content-center align-items-center text-white"></i>
-                  </div>
-                </div>
-                <div
-                  data-bs-toggle="offcanvas"
-                  data-bs-target="#chatoffcanvasRight"
-                  aria-controls="chatoffcanvasRight"
-                >
-                  <div>
-                    <i className="fa-solid  fa-message corner fa-2x  round-img  d-flex justify-content-center align-items-center text-white"></i>
+                    <i className="fa-solid  fa-circle-info corner fa-2x  round-img  d-flex justify-content-center align-items-center text-white"></i>
                   </div>
                 </div>
               </div>
             </div>
+            <div className="d-flex align-items-center justify-content-center col">
+              <div
+                className="me-2"
+                onClick={() => {
+                  mute(myStreamId, myStreamVideo);
+                }}
+              >
+                {isMuted ? (
+                  <i className="fa-solid  fa-microphone-slash call-end bg-danger round-img  d-flex justify-content-center align-items-center text-white"></i>
+                ) : (
+                  <i className="fa-solid  fa-microphone unmute bg-secondary round-img  d-flex justify-content-center align-items-center text-white"></i>
+                )}
+              </div>
+              <div
+                className="me-2"
+                onClick={() => {
+                  stopVideo(myStreamId, myStreamVideo);
+                }}
+              >
+                {isDisplay ? (
+                  <i className="fa-solid  fa-video-slash call-end bg-danger round-img  d-flex justify-content-center align-items-center text-white"></i>
+                ) : (
+                  <i className="fa-solid  fa-video call-end bg-secondary round-img  d-flex justify-content-center align-items-center text-white"></i>
+                )}
+              </div>
+              <div onClick={closeMeeting}>
+                <i className="fa-solid  fa-phone-slash call-end bg-danger round-img  d-flex justify-content-center align-items-center text-white"></i>
+              </div>
+            </div>
+            <div className="d-flex col justify-content-center me-3">
+              <div
+                data-bs-toggle="offcanvas"
+                data-bs-target="#offcanvasRight"
+                aria-controls="offcanvasRight"
+              >
+                <div>
+                  <i className="fa-solid  fa-user corner fa-2x  round-img  d-flex justify-content-center align-items-center text-white"></i>
+                </div>
+              </div>
+              <div
+                data-bs-toggle="offcanvas"
+                data-bs-target="#chatoffcanvasRight"
+                aria-controls="chatoffcanvasRight"
+              >
+                <div>
+                  <i className="fa-solid  fa-message corner fa-2x  round-img  d-flex justify-content-center align-items-center text-white"></i>
+                </div>
+              </div>
+            </div>
           </div>
+        </div>
       </div>
     </>
   );
