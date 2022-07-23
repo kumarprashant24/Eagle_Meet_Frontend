30725817726381a968c2168111724fa586a872e6
kumarprashant24 <kumar.prashant@tothenew.com>
2022-07-23 14:03:19 +0530

    changes in config

--- src/config.js
+++ src/config.js
@@ -4,5 +4,5 @@ if (process.env.REACT_APP_STAGE === 'prod') {
     SERVER_URL = 'https://eagle-meet-backend.herokuapp.com';
 }
 if (process.env.REACT_APP_STAGE === 'dev') {
-    SERVER_URL = 'https://eagle-meet-backend.herokuapp.com'
+    SERVER_URL = 'http://localhost:5000'
 }
