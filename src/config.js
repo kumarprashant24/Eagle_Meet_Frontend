f4cd1255afc24a644b3467b6f265a5faf040401d
kumarprashant24 <kumar.prashant@tothenew.com>
2022-12-20 13:24:47 +0530

    change in config

--- src/config.js
+++ src/config.js
@@ -1,12 +1,8 @@
 export let SERVER_URL;
 if (process.env.REACT_APP_STAGE === 'prod') {
 
-<<<<<<< HEAD
-    SERVER_URL = 'https://eagle-meet-app.onrender.com';
-=======
     SERVER_URL = 'https://eagle-meet-server.herokuapp.com';
     
->>>>>>> origin/meeting
 }
 if (process.env.REACT_APP_STAGE === 'dev') {
     SERVER_URL = 'http://localhost:5000'
